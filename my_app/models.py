d7c72aeff1537d8ca6229c900a0b04bee4c2526a
muratozkan79 <muratozkan79@gmail.com>
2023-05-09 16:22:48 +0300

    Models and Students created

--- my_app/models.py
+++ my_app/models.py
@@ -8,3 +8,11 @@ class Student(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.age}'
+    
+    class Meta:
+        verbose_name = 'Öğrenci'
+        verbose_name_plural = 'Öğrenciler'
+        ordering = ['age']
